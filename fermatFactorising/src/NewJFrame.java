6cc4f27a2596f809d7718736061cbd7708e497d4
jt2friend <jt2friend@gmail.com>
2018-12-11 17:57:41 +0000

    added bigIntegers to fermat factorising and cleaned brute force

--- fermatFactorising/src/NewJFrame.java
+++ fermatFactorising/src/NewJFrame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.math.BigInteger;
 /**
  *
  * @author jt2-friend
@@ -85,7 +85,29 @@ public class NewJFrame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        fermatFactorise(Integer.parseInt(jTextField1.getText()));
+//        fermatFactorise(Integer.parseInt(jTextField1.getText()));
+        String s[] =  {"224573", //this 311 seconds
+                        "299203", //is 0 seconds
+                        "1963867",// very 316 seconds
+                        "6207251",// a while
+                        "14674291",
+                        "23128513",
+                        "254855791",
+                        "428279361"};
+//                        "159649552547",
+//                        "189061250479",
+//                        "2211744201787",
+//                        "7828669742987",
+//                        "48560209712519",
+//                        "35872004189003",
+//                        "737785058178599",
+//                        "576460921650883",
+//                        "1957432135202107",
+//                        "2450609331732137"};
+        
+        for(int i = 0; i < 8; i++) {
+            fermatFactorise(Integer.parseInt(s[i]));
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -123,9 +145,9 @@ public class NewJFrame extends javax.swing.JFrame {
         });
     }
     
-    public double fermatFactorise(int val) {
+    public double fermatFactorise(long val) {
         jTextArea1.append(String.valueOf(val) + " = ");
-        int a = (int)(Math.sqrt(val)) + 1;
+        long a = (long)(Math.sqrt(val)) + 1;
         System.out.println("starting ^2 " + a);
         double possA = 0;
         double x = 0;
@@ -144,13 +166,70 @@ public class NewJFrame extends javax.swing.JFrame {
             }
             a++;
         }
-        int factorA = a - (int)possA;
-        int factorB = a + (int)possA;
+        long factorA = a - (long)possA;
+        long factorB = a + (long)possA;
         jTextArea1.append(String.valueOf(factorA) + " x " + String.valueOf(factorB) + "\n");
         
         return possA;
         //need to make struct to store a and possA;
     }
+    
+    
+    
+    
+    
+    BigInteger getIntSqrt(BigInteger x){
+        BigInteger s; // final result 
+        BigInteger currentRes = BigInteger.valueOf(0); // init value is 0
+        BigInteger currentSum = BigInteger.valueOf(0); // init value is 0
+        BigInteger sum = BigInteger.valueOf(0);
+        String xS = x.toString(); // change input x to a string xS
+       
+        int lengthOfxS = xS.length();
+        int currentTwoBits;
+        int i=0; // index
+        if(lengthOfxS % 2 != 0) {// if odd length, add a dummy bit
+            xS = "0".concat(xS); // add 0 to the front of string xS
+            lengthOfxS++;
+        }
+            
+        while(i < lengthOfxS){ // go through xS two by two, left to right
+            currentTwoBits = Integer.valueOf(xS.substring(i,i+2));
+            i += 2;
+           
+            // sum = currentSum*100 + currentTwoBits
+            sum = currentSum.multiply(BigInteger.valueOf(100));
+            sum = sum.add(BigInteger.valueOf(currentTwoBits));
+            // subtraction loop
+            do {
+                currentSum = sum; // remember the value before subtract
+                // in next 3 lines, we work out currentRes = sum - 2*currentRes - 1
+                sum = sum.subtract(currentRes);
+                currentRes = currentRes.add(BigInteger.valueOf(1)); // currentRes++
+                sum = sum.subtract(currentRes);
+               
+            } while(sum.compareTo(BigInteger.valueOf(0)) >= 0); // stop when sum < 0
+            currentRes = currentRes.subtract(BigInteger.valueOf(1)); // go one step back
+            currentRes = currentRes.multiply(BigInteger.valueOf(10));
+        }
+        s = currentRes.divide(BigInteger.valueOf(10)); // go one step back
+        return s;
+
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
